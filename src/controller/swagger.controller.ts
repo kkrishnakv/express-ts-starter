a2aed199b3c8bfc7f951f8319fd61d5f6bd52faa
Krishna <kv_krishsna@hotmail.com>
2018-06-20 20:59:49 +0530

    swagger issue fixed

--- src/controller/swagger.controller.ts
+++ src/controller/swagger.controller.ts
@@ -6,9 +6,8 @@ const customer = require('../swagger-docs/customer.swagger.json');
 export class SwaggerController {
     public static route = '/swagger';
     public router: Router = Router();
-    private showExplorer = false;
 
     constructor(app: Express) {
-        app.use('/swagger/customer', swaggerUi.serve, swaggerUi.setup(customer, this.showExplorer));
+        app.use('/swagger/customer', swaggerUi.serve, swaggerUi.setup(customer));
     }
 }
