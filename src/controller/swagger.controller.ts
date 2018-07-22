3c3eb7b194d6b58eca9f43114f778b94f446079e
Krishna <kv_krishsna@hotmail.com>
2018-07-22 11:28:55 +0530

    Integrated swagger

---
+++ src/controller/swagger.controller.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response, NextFunction, Express } from 'express';
+import { AppSetting } from './../config';
+const swaggerUi = require('swagger-ui-express');
+import * as customer from '../swagger-docs/customer.swagger.json';
+
+export class SwaggerController {
+    public static route = '/swagger';
+
+    public static configure(app: Express) {
+        app.use('/swagger/customer', swaggerUi.serve, swaggerUi.setup(customer));
+    }
+}
