fef428d7297a57dd631dc4dffcaf1b4411fa1905
krishna <krishna.janardhanan@iff.com>
2020-06-26 09:49:06 +0530

    Version upgrade

--- src/helpers/sequelize/sequelize.config.ts
+++ src/helpers/sequelize/sequelize.config.ts
@@ -1,19 +1,23 @@
-import { AppSetting, IConfig } from '../../config';
-import * as SqlConnection from 'sequelize';
-import { ConfigManager } from '../../config/config.manager';
+import { AppSetting, IConfig } from "../../config";
+import { Sequelize } from "sequelize";
 export class SequelizeConfig {
-    private sequelize: SqlConnection.Sequelize;
-    public setConnection() {
-        const config: IConfig = AppSetting.getConfig();
-        const dbInfo = config.dbConnections['default'];
-        let options = dbInfo.options;
-        options['logging'] = console.log;
-        this.sequelize = new SqlConnection(dbInfo.database, dbInfo.user, dbInfo.password, options);
-    }
+  private sequelize: Sequelize;
+  public setConnection() {
+    const config: IConfig = AppSetting.getConfig();
+    const dbInfo = config.dbConnections["default"];
+    let options = dbInfo.options;
+    options["logging"] = console.log;
+    this.sequelize = new Sequelize(
+      dbInfo.database,
+      dbInfo.user,
+      dbInfo.password,
+      options
+    );
+  }
 
-    public getSequelize() {
-        return this.sequelize;
-    }
+  public getSequelize() {
+    return this.sequelize;
+  }
 }
 
 export const sequelize = new SequelizeConfig();
--- src/helpers/sequelize/sql.manager.ts
+++ src/helpers/sequelize/sql.manager.ts
@@ -1,61 +1,74 @@
-import { Logger } from '../../helpers/logger';
-import { sequelize, SequelizeConfig } from './sequelize.config';
-import * as SqlConnection from 'sequelize';
+import { Logger } from "../../helpers/logger";
+import { sequelize, SequelizeConfig } from "./sequelize.config";
+import { QueryTypes } from "sequelize";
+import * as SqlConnection from "sequelize";
 
 export class SqlManager {
+  private _sequelize: SqlConnection.Sequelize;
+  private params;
 
-    private _sequelize: SqlConnection.Sequelize;
-    private params;
+  constructor() {
+    this._sequelize = sequelize.getSequelize();
+  }
 
-    constructor() {
-        this._sequelize = sequelize.getSequelize();
+  public addInputParameter(paramName: string, value: any) {
+    if (this.params == null) {
+      this.params = {};
     }
+    this.params[paramName] = value;
+  }
 
-    public addInputParameter(paramName: string, value: any) {
-        if (this.params == null) {
-            this.params = {};
-        }
-        this.params[paramName] = value;
-    }
+  public ExecuteQuery(qry: string) {
+    return this._sequelize.query(qry, {
+      type: QueryTypes.SELECT
+    });
+  }
 
+  public Get(qry: string, vals: any = null) {
+    let param = this.params ? this.params : vals;
+    return this._sequelize.query(
+      {
+        query: qry,
+        values: param
+      },
+      { type: QueryTypes.SELECT }
+    );
+  }
 
-    public ExecuteQuery(qry: string) {
-        return this._sequelize.query(qry, { type: this._sequelize.QueryTypes.SELECT });
-    }
+  public Insert(qry: string, vals: any = null) {
+    let param = this.params ? this.params : vals;
+    return this._sequelize.query(
+      {
+        query: qry,
+        values: param
+      },
+      { type: QueryTypes.INSERT }
+    );
+  }
 
-    public Get(qry: string, vals: any = null) {
-        let param = this.params ? this.params : vals;
-        return this._sequelize.query({
-            query: qry,
-            values: param
-        }, { type: this._sequelize.QueryTypes.SELECT });
-    }
+  public Update(qry: string, vals: any = null) {
+    let param = this.params ? this.params : vals;
+    return this._sequelize.query(
+      {
+        query: qry,
+        values: param
+      },
+      { type: QueryTypes.UPDATE }
+    );
+  }
 
-    public Insert(qry: string, vals: any = null) {
-        let param = this.params ? this.params : vals;
-        return this._sequelize.query({
-            query: qry,
-            values: param
-        }, { type: this._sequelize.QueryTypes.INSERT });
-    }
+  public Delete(qry: string, vals: any = null) {
+    let param = this.params ? this.params : vals;
+    return this._sequelize.query(
+      {
+        query: qry,
+        values: param
+      },
+      { type: QueryTypes.DELETE }
+    );
+  }
 
-    public Update(qry: string, vals: any = null) {
-        let param = this.params ? this.params : vals;
-        return this._sequelize.query({
-            query: qry,
-            values: param
-        }, { type: this._sequelize.QueryTypes.UPDATE });
-    }
-
-    public Delete(qry: string, vals: any = null) {
-        let param = this.params ? this.params : vals;
-        return this._sequelize.query({
-            query: qry,
-            values: param
-        }, { type: this._sequelize.QueryTypes.DELETE });
-    }
-
-    public Ping() {
-        return this._sequelize.authenticate();
-    }
+  public Ping() {
+    return this._sequelize.authenticate();
+  }
 }
