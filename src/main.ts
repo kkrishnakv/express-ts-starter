bf2a3b1af209db81306cdc7ac608aef1b32f769a
Krishna <kv_krishna@hotmail.com>
2020-07-24 17:24:56 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { ExpressApi } from "./express.api";
-import { sequelize } from "./helpers/sequelize/sequelize.config";
 
 const api = new ExpressApi();
 
 api.run();
-sequelize.setConnection();
+
 const app = api.app;
 export { app };
